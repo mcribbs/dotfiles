f51538be7e79f93ccfeebaca91691db0e563702a
Matt Cribbs <mcribbs@gmail.com>
2025-08-28 20:57:35 -0400

    fix fzf and compinit

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 curl -s https://ohmyposh.dev/install.sh | bash -s
-sudo apt install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 sudo apt install zoxide
 sudo apt install fortune
 sudo apt install cowsay
