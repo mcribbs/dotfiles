f0897f18f632d45251b9fba3c518ceeb66fe420c
Matt Cribbs <mcribbs@gmail.com>
2025-08-30 17:38:13 -0400

    Update install-packages.sh

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
+
+# Make sure packages are up to date
+sudo apt update
+
+# Install zsh and set default
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# Oh my posh prompt
 curl -s https://ohmyposh.dev/install.sh | bash -s
+
+# fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-sudo apt install zoxide
+
+# zoxide
+#sudo apt install zoxide
+
+# Fun for the welcome message
 sudo apt install fortune
 sudo apt install cowsay
